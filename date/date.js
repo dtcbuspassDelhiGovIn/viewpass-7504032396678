8767359c37b3f6013963609f9156d8eca3472669
Thakur <thakurdutt@THAKURs-MacBook-Air.local>
2023-04-25 00:21:54 +0530

    exchanging all files

---
+++ date/date.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+    document.getElementById('start-date').innerHTML = "04/2023";
+    document.getElementById('end-date').innerHTML = "07/2023";
+    document.getElementById('pass-no').innerHTML = "7504032396";
+  };
